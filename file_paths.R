c2da41ccf3b54ff31ffd4f231e26034600a85a41
Benjamin Edward deMayo <bdemayo@stanford.edu>
2020-01-29 22:10:29 -0800

    closer to final analyses

--- file_paths.R
+++ file_paths.R
@@ -20,3 +20,32 @@ ses_round_1_ws_path <-
 
 ses_round_1_wg_path <- 
   fs::dir_ls(path = path(project_root, "data", "raw", "ses_round_1", "wg"))
+
+ses_round_1_aggregated_wg <- 
+  fs::path(
+    project_root, 
+    "data", 
+    "raw", 
+    "aggregated", 
+    "norming_wg_01_20andNov.summary.csv"
+  )
+
+ses_round_1_aggregated_ws <- 
+  fs::path(
+    project_root, 
+    "data", 
+    "raw", 
+    "aggregated", 
+    "norming_ws_janandnov.csv"
+  )
+
+ses_pilot_wg <- 
+  fs::path(
+    project_root,
+    "data",
+    "raw",
+    "aggregated",
+    "norming_pilot_wg_6_19_summary.csv"
+  )
+
+
